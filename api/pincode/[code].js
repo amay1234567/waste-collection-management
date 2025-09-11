fe2404c9607b2ced48905915d5213dc1d1a89e38
Amay <amaynaik2005@gmail.com>
2025-09-11 20:38:29 +0530

    vercel 4

---
+++ api/pincode/[code].js
@@ -0,0 +1,17 @@
+// pages/api/pincode/[code].js
+
+const pincodeData = {
+  "560001": { area: "Bangalore GPO", city: "Bangalore", state: "Karnataka" },
+  "110001": { area: "Connaught Place", city: "New Delhi", state: "Delhi" },
+  "400001": { area: "Fort", city: "Mumbai", state: "Maharashtra" },
+};
+
+export default function handler(req, res) {
+  const { code } = req.query;  // gets the dynamic part of the URL
+
+  if (pincodeData[code]) {
+    res.status(200).json(pincodeData[code]);
+  } else {
+    res.status(404).json({ error: "Pincode not found" });
+  }
+}
